b779b3e0a3f5e6d1e62d9d4df3f2efa2a4475b6d
jun621 <38369375+jun621@users.noreply.github.com>
2023-06-18 18:15:03 -0700

    Delete identity.c

--- identity.c
+++
@@ -1,8 +0,0 @@
-#include <linux/kernel.h>
-#include <linux/syscalls.h>
-SYSCALL_DEFINE0(identity)
-{
-    
-    printk("This is the new system call Sagario Koizumi implemented"); 
-    return 0; 
-}
